a6bb7a313a4e1904411a518cd8cbe33e2e61c421
MarcosAndre28 <marcosandre1728@gmail.com>
2022-03-08 15:18:54 -0300

    correção chave api

--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsapp.utils
 
 class Constants {
     companion object{
-        const val  API_KEY = "2ce5f928e4954dd2adc6e3c0234bc733"
+        const val  API_KEY = "Chave da api newsapi"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_TIME_DELAY = 500L
     }
